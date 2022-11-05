86be2d7c5c141cfebc93854ec989299cad682b08
loginfinite <74436798+loginfinite@users.noreply.github.com>
2022-11-05 18:15:28 +0800

    final fix

--- stage1/movieSpider.py
+++ stage1/movieSpider.py
@@ -1,15 +1,13 @@
 import requests
 import re
 import fake_useragent
-import json
 import threading
-from threading import Lock
-from queue import Queue
-from time import sleep
+import json
 from lxml import etree
+from queue import Queue
+from threading import Lock
 
 
-MOVIE_URL_PREFIX = "https://movie.douban.com/subject/"
 
 def page_parser_movie(response):
     content = response.text
@@ -49,73 +47,78 @@ def page_parser_movie(response):
 
 
 def get_proxy():
-    while 1:
-        try:
-            PROXY_API_URL = "https://h.shanchendaili.com/api.html?action=get_ip&key=HUe820e7971028093030zNPr&time=10" \
-                            "&count=1&protocol=http&type=json&only=1 "
-            proxy_json = requests.get(PROXY_API_URL).text
-            server = re.findall("(?<=\"sever\":\").*?(?=\")",
-                                proxy_json)[0]
-            port = re.findall("(?<=\"port\":).*?(?=,)",
-                              proxy_json)[0]
-            host = server + ":" + port
-            break
-        except:
-            host = None
-            print("Can't fetch proxy. Retrying\n")
-            sleep(3)
-            continue
-
+    try:
+        PROXY_API_URL = "https://h.shanchendaili.com/api.html?action=get_ip&key=HUe820e7971028093030zNPr&time=10" \
+                        "&count=1&protocol=http&type=json&only=1 "
+        proxy_json = requests.get(PROXY_API_URL).text
+        server = re.findall("(?<=\"sever\":\").*?(?=\")",
+                            proxy_json)[0]
+        port = re.findall("(?<=\"port\":).*?(?=,)",
+                          proxy_json)[0]
+        host = server + ":" + port
+    except:
+        return None
     proxy = {
         'http': 'http://' + host,
         'https': 'https://' + host,
     }
     return proxy
 
+
 def spider(header, lock, queue, finish_num, cookie=None):
     proxy = get_proxy()
 
     while not queue.empty():
-
         _url = queue.get()
-        same_retry = 0
+        same_proxy_retry = 0
         retry = 0
-        book_info = None
+        movie_info = None
+        header['User-Agent'] = str(fake_useragent.UserAgent().random)
 
         while retry < 2:
+
             try:
                 response = requests.get(_url, headers=header, cookies=cookie, proxies=proxy, timeout=8)
-                book_info = page_parser_movie(response)
+                movie_info = page_parser_movie(response)
                 break
-            except Exception as e:
+
+            except Exception as e:  # 同一ip爬取失败达到3次则更换一次ip,若再失败就放弃该url
                 print("\n")
                 print(e)
-                if same_retry < 3:
-                    print("Parser error:retrying for the {re} time(s)\n".format(re=same_retry + 1))
-                    same_retry += 1
-                    continue
-                else:
-                    print("Parser error:change proxy\n".format(re=retry + 1))
-                    proxy = get_proxy()
-                    retry += 1
-                    same_retry = 0
 
-        if book_info is not None:
+                if same_proxy_retry < 3:
+                    print("Crawling error:retrying for {re} time(s)\n".format(re=same_proxy_retry + 1))
+                    same_proxy_retry += 1
+                    continue
+
+                else:
+                    same_proxy_retry = 0
+                    retry += 1
+                    print("Crawling error:change proxy\n".format(re=retry + 1))
+                    proxy = get_proxy()
+
+        if movie_info is not None:
             lock.acquire()
-            with open("./Movie_info.json", 'a') as f:
-                json.dump([book_info], f, indent=4)
-            with open("./Checkpoint_MPVIE.txt", "a") as ckpt:
+            with open("./Movie_info.json", 'a') as info_f:
+                json.dump([movie_info], info_f, indent=4)
+            with open("./Checkpoint_movie.txt", "a") as ckpt:
                 ckpt.write("\n" + _url)
             finish_num[0] += 1
             print('\nProcess:{done}/{Total}\n'.format(done=finish_num[0], Total=1000))
             lock.release()
 
+
 if __name__ == "__main__":
+
+    MOVIE_URL_PREFIX = "https://movie.douban.com/subject/"
+
     with open("./Movie_id.txt", 'r') as f:
         book_url_list = [MOVIE_URL_PREFIX + movie_id.rstrip('\n') + "/" for movie_id in f]
 
-    with open("./Checkpoint_movie.txt", 'r+', encoding='utf-8') as f:
+    with open("./Checkpoint_movie.txt", 'r+', encoding='utf-8') as f:  # checkpoint保存已经爬取成功的url
         url_complete = [_.strip("\n") for _ in f]
+
+    book_url_list = list(set(book_url_list) - set(url_complete)) # 去掉已经爬取过的部分
 
     # try:
     #     with open('cookie.json', 'r', encoding='utf-8') as a:
@@ -123,26 +126,24 @@ if __name__ == "__main__":
     # except:
     #     cookie = None
 
-    book_url_list = list(set(book_url_list) - set(url_complete))
     Book_queue = Queue()
     for i in book_url_list:
         Book_queue.put(i)
 
     header = {'User-Agent': str(fake_useragent.UserAgent().random),
-               'Host': "book.douban.com",
-               "Sec-Fetch-Dest": "document",
-               "Sec-Fetch-Mode": "navigate",
-               "Sec-Fetch-Site": "none",
-               "Sec-Fetch-User": "?1"}
+              'Host': "movie.douban.com",
+              "Sec-Fetch-Dest": "document",
+              "Sec-Fetch-Mode": "navigate",
+              "Sec-Fetch-Site": "none",
+              "Sec-Fetch-User": "?1"}
 
-    t_list = []
+    thread_list = []
     finish_num = [0]
     MAX_THREAD_NUM = 8
-    lock = Lock()
+    file_lock = Lock()
     for i in range(0, MAX_THREAD_NUM):
-        t = threading.Thread(target=spider, args=[header, lock, Book_queue, finish_num])
-        t_list.append(t)
+        t = threading.Thread(target=spider, args=[header, file_lock, Book_queue, finish_num])
+        thread_list.append(t)
         t.start()
-    for t in t_list:
-        t.join()
-
+    for thread in thread_list:
+        thread.join()
